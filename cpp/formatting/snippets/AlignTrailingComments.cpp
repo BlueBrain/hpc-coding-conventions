e3255dd4614c1e496aedb9b0ea201a57bd60b5c5
Tristan Carel <tristan.carel@gmail.com>
2019-01-17 10:25:19 +0100

    Take @ferdonline clang-format remarks into account

--- cpp/formatting/snippets/AlignTrailingComments.cpp
+++ cpp/formatting/snippets/AlignTrailingComments.cpp
@@ -1,4 +1,4 @@
-// Align trailing comments
+// Align trailing comments, prefixed with 2 spaces
 
-int a;     // My comment for a
-int b = 2; // comment b
+int a;      // My comment for a
+int b = 2;  // comment b
