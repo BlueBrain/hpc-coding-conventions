f88f345b9771bc385f676545d4f33bd8800e0ec0
Tristan Carel <tristan.carel@epfl.ch>
2020-12-14 18:51:11 +0100

    apply my comments

---
+++ cpp/cmake/3rdparty.cmake
@@ -0,0 +1,38 @@
+find_package(Git QUIET)
+
+if(NOT ${CODING_CONV_PREFIX}_THIRD_PARTY_DIR)
+  set(${CODING_CONV_PREFIX}_THIRD_PARTY_DIR 3rdparty)
+  set(${CODING_CONV_PREFIX}_THIRD_PARTY_DIR
+      3rdparty
+      PARENT_SCOPE)
+endif()
+
+# initialize submodule with given path
+function(bbp_init_git_submodule path)
+  if(NOT ${GIT_FOUND})
+    message(
+      FATAL_ERROR "git not found and ${path} submodule not cloned (use git clone --recursive)")
+  endif()
+  message(
+    STATUS "Fetching git submodule ${path}: running git submodule update --init --recursive ${path}"
+  )
+  execute_process(
+    COMMAND
+      ${GIT_EXECUTABLE} submodule update --init --recursive -- ${path}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    RESULT_VARIABLE git_submodule_status)
+  if(NOT git_submodule_status EQUAL 0)
+    message(FATAL_ERROR "Could not clone git submodule ${path}")
+  endif()
+endfunction()
+
+# check for external project and initialize submodule if it is missing
+function(bbp_add_git_submodule_subdirectory name)
+  set(submodule_path "${${CODING_CONV_PREFIX}_THIRD_PARTY_DIR}/${name}")
+  if(NOT EXISTS ${submodule_path}/CMakeLists.txt)
+    bbp_init_git_submodule("${submodule_path}")
+  endif()
+  message(STATUS "Sub-project: using ${name} from \"${submodule_path}\"")
+  list(REMOVE_AT ARGV 0)
+  add_subdirectory(${submodule_path} ${ARGV})
+endfunction()
