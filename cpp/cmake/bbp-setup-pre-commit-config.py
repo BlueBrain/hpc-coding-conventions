e465ebd8cec5dd5589250515de80943e63a6d249
Tristan Carel <tristan.carel@epfl.ch>
2021-01-07 18:34:50 +0100

    Add support for git push hooks / add hook to print courtesy message
    * Refactor the CMake options as well as the Python script used to
      generate the pre-commit configuration file to support the
      ability to launch tasks before pushing code.
      For instance, it is now possible to run clang-format
      and/or cmake-format when doing `git push` instead of
      at each commit like before.
    * Introduce a new kind of hook to write a courtesy message
      to the console.
    * patch scripts generated by pre-commit in .git/hooks to fix the shebang
      which can lead to crashes when working in a `spack build-env` environment
    
    Fix #81

--- cpp/cmake/bbp-setup-pre-commit-config.py
+++ cpp/cmake/bbp-setup-pre-commit-config.py
@@ -1,5 +1,10 @@
 import argparse
+from collections import namedtuple
+import copy
+import logging
+import os
 import os.path as osp
+import sys
 
 import yaml
 
@@ -8,115 +13,184 @@ try:
 except ImportError:
     from yaml import Loader
 
-from cpplib import str2bool
-
-
 HPC_PRE_COMMITS_REPO_URL = "https://github.com/BlueBrain/hpc-pre-commits"
 DEFAULT_HPC_PRE_COMMIT_REPO = dict(
     repo=HPC_PRE_COMMITS_REPO_URL, rev="master", hooks=[]
 )
-CHECK_CLANG_FORMAT_HOOK_ID = "check-clang-format"
-CHECK_CMAKE_FORMAT_HOOK_ID = "check-cmake-format"
-CHECK_CLANG_TIDY_HOOK_ID = "clang-tidy"
+
+
+class PreCommitConfig:
+    def __init__(self, file):
+        self._file = file
+        if osp.exists(file):
+            with open(file) as istr:
+                self._config = yaml.load(istr, Loader=Loader) or {}
+            self._previous_config = copy.deepcopy(self._config)
+        else:
+            self._config = {}
+            self._previous_config = {}
+        self._bbp_repo = self._initialize_bbp_repo()
+
+    def _initialize_bbp_repo(self):
+        repos = self.config.setdefault("repos", [])
+        for repo in repos:
+            if repo["repo"] == HPC_PRE_COMMITS_REPO_URL:
+                for k, v in DEFAULT_HPC_PRE_COMMIT_REPO.items():
+                    repo.setdefault(k, v)
+                return repo
+        bbp_repo = DEFAULT_HPC_PRE_COMMIT_REPO
+        repos.append(bbp_repo)
+        return bbp_repo
+
+    @property
+    def config(self):
+        return self._config
+
+    def enable_hook(self, new_hook):
+        logging.info(f"Enable hook {new_hook['name']}")
+        for hook in self._bbp_repo["hooks"]:
+            if (hook["id"], hook.get("name")) == (new_hook["id"], new_hook.get("name")):
+                hook.update(**new_hook)
+                break
+        else:
+            self._bbp_repo["hooks"].append(new_hook)
+
+    def enable_cmake_hook(self, name, stages, args, extra=None):
+        config = dict(
+            id="hpc-pc-cmake-build",
+            args=args,
+            name=name,
+            stages=stages,
+        )
+        if extra:
+            config.update(extra)
+        self.enable_hook(config)
+
+    def enable_cmake_target_hook(self, stages, build_dir, target, **kwargs):
+        self.enable_cmake_hook(
+            target, stages, ["--build", build_dir, "--target", target], **kwargs
+        )
+
+    def enable_cmake_script_hook(self, name, stages, file, **kwargs):
+        self.enable_cmake_hook(name, stages, ["-P", file], **kwargs)
+
+    def disable_cmake_hook(self, name):
+        for i, hook in enumerate(self._bbp_repo["hooks"]):
+            if (name, "hpc-pc-cmake-build") == (hook.get("name"), hook["id"]):
+                self._bbp_repo["hooks"].pop(i)
+                break
+
+    def save(self):
+        if self._previous_config != self.config:
+            logging.info("Updating pre-commit config file: %s", self._file)
+            with open(self._file, "w") as ostr:
+                yaml.dump(self.config, ostr, default_flow_style=False)
+        else:
+            logging.info("pre-commit config is up to date: %s", self._file)
+
+
+class CMakeTargetHook(namedtuple("CMakeTargetHook", ["build_dir", "target"])):
+    def enable(self, config, stages):
+        config.enable_cmake_target_hook(
+            stages, self.build_dir, self.target, extra=dict(always_run=True)
+        )
+
+    def disable(self, config):
+        config.disable_cmake_hook(self.target)
+
+    @property
+    def name(self):
+        return self.target
+
+
+class CMakeScriptHook(namedtuple("CMakeScriptHook", ["name", "file"])):
+    def enable(self, config, stages):
+        config.enable_cmake_script_hook(
+            self.name, stages, self.file, extra=dict(always_run=True, verbose=True)
+        )
+
+    def disable(self, config):
+        config.disable_cmake_hook(self.name)
 
 
 def _parse_cli(args=None):
     parser = argparse.ArgumentParser(description="Ensure CMake files formatting")
     parser.add_argument(
-        "--clang-format",
-        type=str2bool,
-        help="Enable C/C++ code formatting check",
-        const=True,
-        default=False,
-        nargs="?",
+        "--commit-checks",
+        help="Comma-separated list of checks to perform when committing changes",
+        default="",
     )
     parser.add_argument(
-        "-f",
-        "--force",
-        type=str2bool,
-        help="Force regenerating pre-commit hooks",
-        default=False,
-    )
-    parser.add_argument(
-        "--cmake-format", type=str2bool, help="Enable CMake files code formatting check"
-    )
-    parser.add_argument(
-        "--clang-tidy",
-        type=str2bool,
-        help="Enable static-analysis check with Clang Tidy",
+        "--push-checks",
+        help="Comma-separated list of checks to perform when pushing changes",
+        default="",
     )
     parser.add_argument("source_dir", help="CMake source directory")
     parser.add_argument("build_dir", help="CMake binary directory")
     return parser.parse_args(args=args)
 
 
-def get_or_set_bbp_pre_commit_repo(config):
-    repos = config.setdefault("repos", [])
-    for repo in repos:
-        if repo["repo"] == HPC_PRE_COMMITS_REPO_URL:
-            for k, v in DEFAULT_HPC_PRE_COMMIT_REPO.items():
-                repo.setdefault(k, v)
-            return repo
-    bbp_repo = DEFAULT_HPC_PRE_COMMIT_REPO
-    repos.append(bbp_repo)
-    return bbp_repo
-
-
-def enable_hook(repo, new_hook):
-    for hook in repo["hooks"]:
-        if (hook["id"], hook.get("name")) == (new_hook["id"], new_hook.get("name")):
-            hook.update(**new_hook)
-            break
-    else:
-        repo["hooks"].append(new_hook)
-
-
-def enable_cmake_hook(repo, build_dir, target):
-    enable_hook(
-        repo,
-        dict(
-            id="hpc-pc-cmake-build",
-            args=["--build", build_dir, "--target", target],
-            name=target,
-        ),
-    )
-
-
-def disable_cmake_hook(repo, hook_id):
-    for i, hook in enumerate(repo["hooks"]):
-        if (hook_id, "hpc-pc-cmake-build") == (hook.get("name"), hook["id"]):
-            repo["hooks"].pop(i)
-            break
+def fix_hook_file(hook):
+    if not osp.exists(hook):
+        return
+    with open(hook) as istr:
+        lines = istr.readlines()
+    shebang = f"#!/usr/bin/env {sys.executable}\n"
+    if lines[0] == "#!/usr/bin/env python\n":
+        logging.warning(f"Patching git hook script: {hook}")
+        lines[0] = shebang
+        with open(hook, "w") as ostr:
+            ostr.writelines(lines)
 
 
 def main(**kwargs):
     args = _parse_cli(**kwargs)
     PRE_COMMIT_CONFIG = osp.join(args.source_dir, ".pre-commit-config.yaml")
-    if osp.exists(PRE_COMMIT_CONFIG):
-        if not args.force:
-            return
-        else:
-            with open(PRE_COMMIT_CONFIG) as istr:
-                config = yaml.load(istr, Loader=Loader) or {}
-    else:
-        config = {}
+    ALL_HOOKS = [
+        CMakeTargetHook(args.build_dir, "check-clang-format"),
+        CMakeTargetHook(args.build_dir, "check-cmake-format"),
+        CMakeTargetHook(args.build_dir, "clang-tidy"),
+        CMakeScriptHook(
+            "courtesy-msg", osp.join(args.build_dir, "git-push-message.cmake")
+        ),
+    ]
 
-    repo = get_or_set_bbp_pre_commit_repo(config)
-    if args.clang_format:
-        enable_cmake_hook(repo, args.build_dir, CHECK_CLANG_FORMAT_HOOK_ID)
-    else:
-        disable_cmake_hook(repo, CHECK_CLANG_FORMAT_HOOK_ID)
-    if args.cmake_format:
-        enable_cmake_hook(repo, args.build_dir, CHECK_CMAKE_FORMAT_HOOK_ID)
-    else:
-        disable_cmake_hook(repo, CHECK_CMAKE_FORMAT_HOOK_ID)
-    if args.clang_tidy:
-        enable_cmake_hook(repo, args.build_dir, CHECK_CLANG_TIDY_HOOK_ID)
-    else:
-        disable_cmake_hook(repo, CHECK_CLANG_TIDY_HOOK_ID)
-    with open(PRE_COMMIT_CONFIG, "w") as ostr:
-        yaml.dump(config, ostr, default_flow_style=False)
+    config = PreCommitConfig(PRE_COMMIT_CONFIG)
+
+    hooks = dict((hook.name, hook) for hook in ALL_HOOKS)
+    assert len(hooks) == len(ALL_HOOKS), "hooks must have unique names"
+
+    # Build a dictionary from the two CLI arguments
+    # For example: {"clang-tidy": ["commit"], "courtesy-msg": ["commit", "push"]}
+    hook_stages = {}
+    for names, stage in [(args.commit_checks, "commit"), (args.push_checks, "push")]:
+        for name in names.split(","):
+            name = name.strip()
+            if name:
+                hook_stages.setdefault(name, []).append(stage)
+
+    # Enable hooks mentioned in `hook_stages`
+    for name, stages in hook_stages.items():
+        hook = hooks.pop(name, None)
+        if hook is None:
+            logging.warning(
+                f"Unknown check named: '{name}', "
+                f"available checks: {', '.join(hooks.keys())}"
+            )
+            continue
+        hook.enable(config, stages)
+
+    # Disable the other ones
+    for hook in hooks.values():
+        hook.disable(config)
+
+    config.save()
+
+    fix_hook_file(osp.join(args.source_dir, ".git", "hooks", "pre-commit"))
+    fix_hook_file(osp.join(args.source_dir, ".git", "hooks", "pre-push"))
 
 
 if __name__ == "__main__":
+    level = logging.INFO if "VERBOSE" in os.environ else logging.WARN
+    logging.basicConfig(level=level, format="%(message)s")
     main()
