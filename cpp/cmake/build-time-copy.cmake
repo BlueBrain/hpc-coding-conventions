91c5d922a1b45943c6d2eca719088b547243f75c
Olli Lupton <oliver.lupton@epfl.ch>
2021-04-06 16:20:43 +0200

    Add NO_TARGET option to build time copy helper. (#96)
    By default cpp_cc_build_time_copy creates a target that is always built
    that depends on the output file, ensuring that it is already up to date.
    This potentially generates a large number of top-level always-built
    targets, which can be rather slow when using legacy make/Makefiles. The
    Ninja build system does not seem to have the same problem. This commit
    adds an optional NO_TARGET flag, which sets up the rule to copy the file
    but does not create a top-level always-built target. This means that the
    caller can create a single such target depending on many files, which is
    much more efficient when using make/Makefiles.

--- cpp/cmake/build-time-copy.cmake
+++ cpp/cmake/build-time-copy.cmake
@@ -1,7 +1,8 @@
 # Create a build rule that copies a file.
 #
 # cpp_cc_build_time_copy(INPUT <input_path>
-#                        OUTPUT <output_path>)
+#                        OUTPUT <output_path>
+#                        [NO_TARGET])
 #
 # This creates a custom target that is always built and depends on
 # `output_path` and a rule to create `output_path` by copying `input_path`.
@@ -9,22 +10,30 @@
 # source tree) can be propagated to `output_path` (presumably in the build
 # tree) automatically and without re-running CMake. The existence of a custom
 # command that produces `output_path` makes it trivial for other targets to
-# declare that they depend on this file.
+# declare that they depend on this file, provided that they are working in the
+# same directory (CMakeLists.txt -- see documentation of DEPENDS arguments to
+# add_custom_command and add_custom_target). If the NO_TARGET flag is passed
+# then no target is added, and the caller takes responsibility for declaring a
+# dependency on the output file and causing it to be built. This can be a good
+# idea to avoid the number of top level targets growing too large, which causes
+# the Make build system to be very slow.
 function(cpp_cc_build_time_copy)
-  cmake_parse_arguments(opt "" "INPUT;OUTPUT" "" ${ARGN})
+  cmake_parse_arguments(opt "NO_TARGET" "INPUT;OUTPUT" "" ${ARGN})
   if(NOT DEFINED opt_INPUT)
     message(ERROR "build_time_copy missing required keyword argument INPUT.")
   endif()
   if(NOT DEFINED opt_OUTPUT)
     message(ERROR "build_time_copy missing required keyword argument OUTPUT.")
   endif()
-  string(SHA256 target_name "${opt_INPUT};${opt_OUTPUT}")
-  set(target_name "build-time-copy-${target_name}")
-  if(NOT TARGET "${target_name}")
-    add_custom_command(
-      OUTPUT "${opt_OUTPUT}"
-      DEPENDS "${opt_INPUT}"
-      COMMAND ${CMAKE_COMMAND} -E copy "${opt_INPUT}" "${opt_OUTPUT}")
-    add_custom_target(${target_name} ALL DEPENDS "${opt_OUTPUT}")
+  add_custom_command(
+    OUTPUT "${opt_OUTPUT}"
+    DEPENDS "${opt_INPUT}"
+    COMMAND ${CMAKE_COMMAND} -E copy "${opt_INPUT}" "${opt_OUTPUT}")
+  if(NOT opt_NO_TARGET)
+    string(SHA256 target_name "${opt_INPUT};${opt_OUTPUT}")
+    set(target_name "build-time-copy-${target_name}")
+    if(NOT TARGET "${target_name}")
+      add_custom_target(${target_name} ALL DEPENDS "${opt_OUTPUT}")
+    endif()
   endif()
 endfunction()
